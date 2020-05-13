475782cc111419d706f68c4882d93c55dd874cd0
rad-muk3 <radhika_mukund@hotmail.com>
2020-05-12 20:25:40 -0700

    L2 Ex 15 Mode

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,35 +1,24 @@
 const path = require('path')
 const webpack = require('webpack')
-const HtmlWebPackPlugin = require("html-webpack-plugin")
-const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const HtmlWebPackPlugin = require('html-webpack-plugin')
 
 module.exports = {
+    // Hint: Add something here!
     entry: './src/client/index.js',
-    mode: 'development',
-    devtool: 'source-map',
-    stats: 'verbose',
-    module: {
-        rules: [
-            {
-                test: '/\.js$/',
-                exclude: /node_modules/,
-                loader: "babel-loader"
-            }
-        ]
-    },
-    plugins: [
-        new HtmlWebPackPlugin({
-            template: "./src/client/views/index.html",
-            filename: "./index.html",
-        }),
-        new CleanWebpackPlugin({
-            // Simulate the removal of files
-            dry: true,
-            // Write Logs to Console
-            verbose: true,
-            // Automatically remove all unused webpack assets on rebuild
-            cleanStaleWebpackAssets: true,
-            protectWebpackAssets: false
-        })
+	mode: 'production',
+	module: {
+    rules: [
+        {
+            test: '/\.js$/',
+            exclude: /node_modules/,
+            loader: "babel-loader"
+        }
     ]
+   },
+   plugins: [
+        new HtmlWebPackPlugin({
+			template: "./src/client/views/index.html",
+			filename: "./index.html",
+		})
+   ]
 }
